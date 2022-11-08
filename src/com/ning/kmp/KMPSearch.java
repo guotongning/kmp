fd8701c70a6b8f2f1669c5b1db1acfecc10b664f
guotongning <guotongning@58.com>
2022-11-08 10:03:06 +0800

    [refactor] 优化

--- src/com/ning/kmp/KMPSearch.java
+++ src/com/ning/kmp/KMPSearch.java
@@ -16,33 +16,36 @@ public class KMPSearch {
 
 
     public static int kmp(String string, String pattern) {
-        int i = 0, j = 0;
-        int[] next = getNext(pattern);
-        while (i < string.length() && j < pattern.length()) {
-            if (j == -1 || string.charAt(i) == pattern.charAt(j)) {
-                i++;
+        int[] next = getNext(string);
+        for (int i = 1, j = 0; i < string.length(); i++) {
+            while (j > 0 && string.charAt(i) != pattern.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (string.charAt(i) == pattern.charAt(j)) {
                 j++;
-            } else {
-                j = next[j];
+            }
+            if (j == pattern.length()) {
+                return i - j + 1;
             }
         }
-        if (j == pattern.length())
-            return i - j;
-        else
-            return -1;
+        return -1;
     }
 
-    private static int[] getNext(String sub) {
-        int[] next = new int[sub.length() + 1];
-        int i = 0;
-        int j = -1;
-        next[0] = -1;
-        while (i < sub.length()) {
-            if (j == -1 || sub.charAt(i) == sub.charAt(j)) {
-                next[++i] = ++j;
-            } else {
-                j = next[j];
+    public static int[] getNext(String pattern) {
+        int[] next = new int[pattern.length()];
+        //i从1开始是因为，第一个字符没有前后缀，因此相同前后缀最大长度=0
+        //j：当前共同前后缀长度
+        for (int i = 1, j = 0; i < pattern.length(); i++) {
+            //不匹配时，回溯已知信息。不断的判断是否有更小的相同前后缀子串。
+            if (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = next[j - 1];
             }
+            //匹配成功，共同前后缀长度+1
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                j++;
+            }
+            //记录结果
+            next[i] = j;
         }
         return next;
     }
